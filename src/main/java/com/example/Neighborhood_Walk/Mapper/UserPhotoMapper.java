f7673da92f7c8f8dd9067fe2c9e28fdb2bb33432
Nikolaszhou <jiwu0116@uni.sydney.edu.au>
2024-10-19 18:09:39 +1100

    Update for userphoto controller and mapper

--- src/main/java/com/example/Neighborhood_Walk/Mapper/UserPhotoMapper.java
+++ src/main/java/com/example/Neighborhood_Walk/Mapper/UserPhotoMapper.java
@@ -2,6 +2,7 @@ package com.example.Neighborhood_Walk.Mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.Neighborhood_Walk.entity.UserPhoto;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -16,5 +17,8 @@ public interface UserPhotoMapper extends BaseMapper<UserPhoto> {
 
     @Select("SELECT * FROM UserPhotos WHERE user_id = #{userId}")
     List<UserPhoto> findPhotosByUserId(String userId);
+
+    @Delete("DELETE FROM UserPhotos WHERE user_id = #{userId}")
+    void deleteByUserId(String userId);
 }
 
--- src/main/java/com/example/Neighborhood_Walk/controller/FileUploadController.java
+++ src/main/java/com/example/Neighborhood_Walk/controller/FileUploadController.java
@@ -1,5 +1,7 @@
 package com.example.Neighborhood_Walk.controller;
 
+import com.example.Neighborhood_Walk.Mapper.UserPhotoMapper;
+import com.example.Neighborhood_Walk.entity.UserPhoto;
 import com.example.Neighborhood_Walk.service.MinioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -7,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
@@ -15,12 +18,25 @@ public class FileUploadController {
     @Autowired
     private MinioService minioService;
 
+    @Autowired
+    private UserPhotoMapper userPhotoMapper; // 数据库操作
+
     @PostMapping("/upload-avatar")
-    public Map<String, String> uploadAvatar(@RequestParam("avatar") MultipartFile file) {
+    public Map<String, String> uploadAvatar(@RequestParam String userId, @RequestParam("avatar") MultipartFile file) {
         Map<String, String> response = new HashMap<>();
         try {
             // 上传文件并返回 MinIO 中的 URL
             String fileUrl = minioService.uploadFile(file);
+
+            System.out.println(fileUrl);
+            // 将上传后的文件URL保存到数据库
+            UserPhoto userPhoto = new UserPhoto();
+            userPhoto.setPhotoId(UUID.randomUUID().toString());
+            userPhoto.setUserId(userId);
+            userPhoto.setPhotoUrl(fileUrl);
+            userPhotoMapper.deleteByUserId(userId);
+            userPhotoMapper.insertUserPhoto(userPhoto);
+
             response.put("fileUrl", fileUrl);
         } catch (Exception e) {
             response.put("error", "Failed to upload file: " + e.getMessage());
